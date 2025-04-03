41c53cbcecfc906bbefa79c88c42d5ad15d2bcd9
Mr.Y <664960698@qq.com>
2025-04-03 11:02:07 +0800

    add

--- packages/map-app/src/components/Home.vue
+++ packages/map-app/src/components/Home.vue
@@ -6,9 +6,10 @@ import Tile from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
 import BingMaps from 'ol/source/BingMaps'
 import XYZ from 'ol/source/XYZ'
-import { transform } from 'ol/proj'
+import { fromLonLat } from 'ol/proj'
 
 let mapInstance
+const apiKey = '3e3059b01c5c4199fb64f898f30a9c1c'
 
 onMounted(() => {
   initMap()
@@ -22,19 +23,61 @@ function initMap() {
       new Tile({
         source: new XYZ({
           wrapX: false,
-          url: 'http://t0.tianditu.gov.cn/vec_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=3e3059b01c5c4199fb64f898f30a9c1c'
+          tileUrlFunction: function (tileCoord) {
+            debugger
+            var z = tileCoord[0]
+            var x = tileCoord[1]
+            var y = tileCoord[2]
+            if (y === undefined) return ''
+            var y_tms = Math.pow(2, z) - 1 - y
+            return (
+              'http://t0.tianditu.gov.cn/vec_w/wmts?' +
+              'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
+              '&LAYER=vec&STYLE=default&TILEMATRIXSET=w' +
+              '&FORMAT=image/png' +
+              '&TILEMATRIX=' +
+              z +
+              '&TILEROW=' +
+              y +
+              '&TILECOL=' +
+              x +
+              '&tk=' +
+              apiKey
+            )
+          }
         })
       }),
       new Tile({
         source: new XYZ({
           wrapX: false,
-          url: 'http://t0.tianditu.gov.cn/cva_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=3e3059b01c5c4199fb64f898f30a9c1c'
+          tileUrlFunction: function (tileCoord) {
+            const z = tileCoord[0]
+            const x = tileCoord[1]
+            const y = tileCoord[2]
+            if (y === undefined) return ''
+            const y_tms = Math.pow(2, z) - 1 - y // TMS行号反转
+            return (
+              'http://t0.tianditu.gov.cn/cva_w/wmts?' +
+              'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
+              '&LAYER=cva&STYLE=default&TILEMATRIXSET=w' +
+              '&FORMAT=image/png' +
+              '&TILEMATRIX=' +
+              z +
+              '&TILEROW=' +
+              y +
+              '&TILECOL=' +
+              x +
+              '&tk=' +
+              apiKey
+            )
+          }
         })
       })
     ],
     view: new View({
-      center: [0, 0],
-      zoom: 2
+      center: fromLonLat([116.4, 39.9]), // 北京市中心
+      zoom: 2,
+      projection: 'EPSG:3857'
     })
   })
 }
