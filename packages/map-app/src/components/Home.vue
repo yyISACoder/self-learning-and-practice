2d7ac6fe2416617a9dc9bbfc4c266b83856d3cb0
Mr.Y <664960698@qq.com>
2025-04-10 15:59:55 +0800

    add

--- packages/map-app/src/components/Home.vue
+++ packages/map-app/src/components/Home.vue
@@ -3,17 +3,25 @@ import { ref, onMounted } from 'vue'
 import 'ol/ol.css'
 import { Map, View, Feature } from 'ol'
 import Tile from 'ol/layer/Tile'
+import ScaleLine from 'ol/control/ScaleLine'
+import ZoomSlider from 'ol/control/ZoomSlider'
+import FullScreen from 'ol/control/FullScreen'
+import MousePosition from 'ol/control/MousePosition'
+import OverviewMap from 'ol/control/OverviewMap'
 import OSM from 'ol/source/OSM'
 import BingMaps from 'ol/source/BingMaps'
 import XYZ from 'ol/source/XYZ'
+import ImageStatic from 'ol/source/ImageStatic'
+import ImageLayer from 'ol/layer/Image'
 import VectorSource from 'ol/source/Vector'
 import { fromLonLat, transform } from 'ol/proj'
 import VectorLayer from 'ol/layer/Vector'
 import { GeoJSON } from 'ol/format'
-import { LineString } from 'ol/geom'
+import { LineString, Point } from 'ol/geom'
 import { Style, Stroke, Circle, Fill } from 'ol/style'
 import AMapLoader from '@amap/amap-jsapi-loader'
 import { gcj02towgs84 } from 'coordtransform'
+import { Button } from 'ant-design-vue'
 
 let mapInstance
 let trafficLayer = null
@@ -37,114 +45,160 @@ async function initGDMap() {
   })
 }
 function initMap() {
-  debugger
+  const layers = [
+    new Tile({
+      source: new XYZ({
+        url: gaodeTileUrl,
+        wrapX: false
+      })
+    })
+    // new Tile({
+    //   source: new XYZ({
+    //     wrapX: false,
+    //     projection: 'EPSG:3857',
+    //     tileUrlFunction: function (tileCoord) {
+    //       const z = tileCoord[0]
+    //       const x = tileCoord[1]
+    //       const y = tileCoord[2]
+    //       if (y === undefined) return ''
+    //       const y_tms = Math.pow(2, z) - 1 - y // TMS行号反转
+    //       return (
+    //         'http://t0.tianditu.gov.cn/ter_w/wmts?' +
+    //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
+    //         '&LAYER=ter&STYLE=default&TILEMATRIXSET=w' +
+    //         '&FORMAT=image/png' +
+    //         '&TILEMATRIX=' +
+    //         z +
+    //         '&TILEROW=' +
+    //         y +
+    //         '&TILECOL=' +
+    //         x +
+    //         '&tk=' +
+    //         apiKey_td
+    //       )
+    //     }
+    //   })
+    // }),
+    // new VectorLayer({
+    //   opacity: 1,
+    //   source: new VectorSource({
+    //     url: 'https://geo.datav.aliyun.com/areas_v3/bound/geojson?code=100000_full', // GeoJSON文件路径
+    //     format: new GeoJSON(),
+    //     projection: 'EPSG:3857'
+    //   })
+    // }),
+    // new ImageLayer({
+    //   visible: true,
+    //   source: new ImageStatic({
+    //     projection: 'EPSG:4326',
+    //     imageExtent: [100, 30, 102, 32],
+    //     url: 'https://picsum.photos/200'
+    //   })
+    // })
+    // new Tile({
+    //   source: new XYZ({
+    //     wrapX: false,
+    //     projection: 'EPSG:3857',
+    //     tileUrlFunction: function (tileCoord) {
+    //       const z = tileCoord[0]
+    //       const x = tileCoord[1]
+    //       const y = tileCoord[2]
+    //       if (y === undefined) return ''
+    //       const y_tms = Math.pow(2, z) - 1 - y // TMS行号反转
+    //       return (
+    //         'http://t0.tianditu.gov.cn/ter_w/wmts?' +
+    //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
+    //         '&LAYER=ter&STYLE=default&TILEMATRIXSET=w' +
+    //         '&FORMAT=image/png' +
+    //         '&TILEMATRIX=' +
+    //         z +
+    //         '&TILEROW=' +
+    //         y +
+    //         '&TILECOL=' +
+    //         x +
+    //         '&tk=' +
+    //         apiKey_td
+    //       )
+    //     }
+    //   })
+    // }),
+    // new Tile({
+    //   opacity: 0.7,
+    //   source: new XYZ({
+    //     wrapX: false,
+    //     projection: 'EPSG:3857',
+    //     tileUrlFunction: function (tileCoord) {
+    //       debugger
+    //       var z = tileCoord[0]
+    //       var x = tileCoord[1]
+    //       var y = tileCoord[2]
+    //       if (y === undefined) return ''
+    //       var y_tms = Math.pow(2, z) - 1 - y
+    //       return (
+    //         'http://t0.tianditu.gov.cn/img_w/wmts?' +
+    //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
+    //         '&LAYER=img&STYLE=default&TILEMATRIXSET=w' +
+    //         '&FORMAT=image/png' +
+    //         '&TILEMATRIX=' +
+    //         z +
+    //         '&TILEROW=' +
+    //         y +
+    //         '&TILECOL=' +
+    //         x +
+    //         '&tk=' +
+    //         apiKey_td
+    //       )
+    //     }
+    //   })
+    // })
+  ]
   mapInstance = new Map({
     target: 'mapContainer',
-    layers: [
-      // new Tile({
-      //   source: new XYZ({
-      //     wrapX: false,
-      //     projection: 'EPSG:3857',
-      //     tileUrlFunction: function (tileCoord) {
-      //       const z = tileCoord[0]
-      //       const x = tileCoord[1]
-      //       const y = tileCoord[2]
-      //       if (y === undefined) return ''
-      //       const y_tms = Math.pow(2, z) - 1 - y // TMS行号反转
-      //       return (
-      //         'http://t0.tianditu.gov.cn/ter_w/wmts?' +
-      //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
-      //         '&LAYER=ter&STYLE=default&TILEMATRIXSET=w' +
-      //         '&FORMAT=image/png' +
-      //         '&TILEMATRIX=' +
-      //         z +
-      //         '&TILEROW=' +
-      //         y +
-      //         '&TILECOL=' +
-      //         x +
-      //         '&tk=' +
-      //         apiKey_td
-      //       )
-      //     }
-      //   })
-      // }),
-      new VectorLayer({
-        opacity: 1,
-        source: new VectorSource({
-          url: 'https://geo.datav.aliyun.com/areas_v3/bound/geojson?code=100000_full', // GeoJSON文件路径
-          format: new GeoJSON(),
-          projection: 'EPSG:3857'
-        })
-      })
-      // new Tile({
-      //   source: new XYZ({
-      //     url: gaodeTileUrl,
-      //     wrapX: false
-      //   })
-      // })
-      // new Tile({
-      //   source: new XYZ({
-      //     wrapX: false,
-      //     projection: 'EPSG:3857',
-      //     tileUrlFunction: function (tileCoord) {
-      //       const z = tileCoord[0]
-      //       const x = tileCoord[1]
-      //       const y = tileCoord[2]
-      //       if (y === undefined) return ''
-      //       const y_tms = Math.pow(2, z) - 1 - y // TMS行号反转
-      //       return (
-      //         'http://t0.tianditu.gov.cn/ter_w/wmts?' +
-      //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
-      //         '&LAYER=ter&STYLE=default&TILEMATRIXSET=w' +
-      //         '&FORMAT=image/png' +
-      //         '&TILEMATRIX=' +
-      //         z +
-      //         '&TILEROW=' +
-      //         y +
-      //         '&TILECOL=' +
-      //         x +
-      //         '&tk=' +
-      //         apiKey_td
-      //       )
-      //     }
-      //   })
-      // }),
-      // new Tile({
-      //   opacity: 0.7,
-      //   source: new XYZ({
-      //     wrapX: false,
-      //     projection: 'EPSG:3857',
-      //     tileUrlFunction: function (tileCoord) {
-      //       debugger
-      //       var z = tileCoord[0]
-      //       var x = tileCoord[1]
-      //       var y = tileCoord[2]
-      //       if (y === undefined) return ''
-      //       var y_tms = Math.pow(2, z) - 1 - y
-      //       return (
-      //         'http://t0.tianditu.gov.cn/img_w/wmts?' +
-      //         'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0' +
-      //         '&LAYER=img&STYLE=default&TILEMATRIXSET=w' +
-      //         '&FORMAT=image/png' +
-      //         '&TILEMATRIX=' +
-      //         z +
-      //         '&TILEROW=' +
-      //         y +
-      //         '&TILECOL=' +
-      //         x +
-      //         '&tk=' +
-      //         apiKey_td
-      //       )
-      //     }
-      //   })
-      // })
-    ],
+    layers,
     view: new View({
       center: transform([116.4, 39.9], 'EPSG:4326', 'EPSG:3857'), // 北京市中心
       zoom: 4,
       projection: 'EPSG:3857'
+    }),
+    controls: [
+      new FullScreen(), // 全屏控件
+      // new MousePosition({
+      //   coordinateFormat: function (coordinate) {
+      //     return `东经${coordinate?.[0]} 北纬${coordinate?.[1]}`
+      //   }
+      // }), //鼠标位置控件
+      new OverviewMap({
+        layers
+      }),
+      new ZoomSlider(), //缩放滑块刻度控件
+      new ScaleLine({
+        className: 'ol-scale-line custom-zoom-line'
+      })
+    ]
+  })
+}
+function drawUserLocation(lon, lat, accuracy) {
+  // 转换经纬度到地图坐标系（EPSG:3857）
+  const userCoords = fromLonLat([lon, lat])
+
+  // 创建用户位置标记
+  const userMarker = new Feature({
+    geometry: new Point(userCoords)
+  })
+  const markerStyle = new Style({
+    image: new Circle({
+      radius: 6,
+      fill: new Fill({ color: 'rgba(0, 153, 255, 1)' }),
+      stroke: new Stroke({ color: 'white', width: 2 })
     })
   })
+  const vectorLayer = new VectorLayer({
+    source: new VectorSource({
+      features: [userMarker]
+    }),
+    style: [markerStyle]
+  })
+  mapInstance.addLayer(vectorLayer)
 }
 // 3. 加载实时交通数据
 function loadTrafficData() {
@@ -201,10 +255,34 @@ function updateTrafficLayer(roads) {
   debugger
   mapInstance.addLayer(trafficLayer)
 }
+function onGetLocation() {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      // 成功获取位置
+      const lon = position.coords.longitude // 经度（WGS84）
+      const lat = position.coords.latitude // 纬度（WGS84）
+      const accuracy = position.coords.accuracy // 精度（米）
+      // 调用函数将坐标绘制到地图
+      drawUserLocation(lon, lat, accuracy)
+    },
+    (error) => {
+      // 处理错误（用户拒绝授权或定位失败）
+      console.error('获取位置失败：', error.message)
+    },
+    {
+      //enableHighAccuracy: true, // 高精度模式（GPS）
+      //timeout: 5000, // 超时时间（毫秒）
+      maximumAge: 0 // 不缓存位置
+    }
+  )
+}
 </script>
 
 <template>
   <div class="wrapper">
+    <div class="opt-box">
+      <Button type="primary" @click="onGetLocation">获取当前位置</Button>
+    </div>
     <div id="mapContainer" class="map-container"></div>
   </div>
 </template>
--- packages/map-app/vite.config.js
+++ packages/map-app/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/map',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
@@ -10,5 +11,10 @@ export default defineConfig({
         javascriptEnabled: true
       }
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    //https: true,
+    hmr: true
   }
 })
